31fd7ed230344a6205860e7362525637a9b942a8
Tchong-Chen <theo292017@gmail.com>
2024-12-03 12:19:32 +0100

    Add files via upload

--- centre_appel.c
+++ centre_appel.c
@@ -1,7 +1,8 @@
-#include <stdio.lib>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <stdbool.h>
 
 
 // def constantes
@@ -27,25 +28,40 @@ double intervalle_tps(void)
 
 // Simulation tps d'arrivee des clients
 
-int simu_arrivee()
+int* simu_arrivee()
 {
     int i = 0;
-    int tableau_tps_arrivee = (int *) malloc(size_of(int));
+    int* tableau_tps_arrivee = malloc(10000 * sizeof(int));
+    tableau_tps_arrivee[i] = intervalle_tps();
 
-    while(tableau_tps_arrivee[i] < NBR_HEURES * 3600)
+    while(tableau_tps_arrivee[i] < 24 * 3600)
     {
-        int tps = intervalle_tps(void);
-        
+        i++;
+        int tps = intervalle_tps();
+        tableau_tps_arrivee[i] = tableau_tps_arrivee[i - 1] + tps;
     }
+    return tableau_tps_arrivee;
+}
+
+// comparaison entre tableau de simu_arrivee et l'heure actuelle
+
+bool compare_tps(int j, int *tab)
+{
+    int i = 0;
+    while( tab[i] < j)
+        i++;
+    return (tab[i] == j);
 }
 
 
 // tps de prise en charge client
 
-int tps_prise_en_charge(struct T_noeud)
+int tps_prise_en_charge()
 {
-    int tps_avec_client = rand()
+    int tps_avec_client = rand() % maxsrv + minsrv;
+    return tps_avec_client;
 }
+
 
 // creation d'une file sous forme d'une liste chainee
 
@@ -63,12 +79,12 @@ typedef struct Noeud {
 
 // ajout d'un client dans la file
 
-void ajout_client(struct T_noeud *tete, struct Client nv_client)
+void ajout_client(T_noeud *file, struct Client nv_client)
 {
     T_noeud *tete = NULL;
     T_noeud *nouveau;
 
-    nouveau = ((T_noeud *) malloc(size_of(T_noeud)));
+    nouveau = ((T_noeud *) malloc(sizeof(T_noeud)));
 
     nouveau -> data = nv_client;
     nouveau -> suiv = *tete;
@@ -78,56 +94,111 @@ void ajout_client(struct T_noeud *tete, struct Client nv_client)
 
 // retirer un client de la file
 
-void pop_client(struct T_noeud *tete, struct Client *sauvegarde) 
+struct pop_client(T_noeud *tete) 
 {
     if (*tete == NULL)
         return;
 
-    if ((*tete)->suiv == NULL) 
+    if ((*tete) -> suiv == NULL) 
     {
-        *sauvegarde = (*tete)->data;
+        int *sauvegarde = (*tete) -> data;
         free(*tete);
         *tete = NULL;
-        return;
+        return sauvegarde;
     }
 
-    T_noeud *current = *tete;
-    while (current->suiv != NULL) 
-        current = current->suiv;
+    T_noeud *courant = *tete;
+    while (courant -> (suiv -> suiv) != NULL) 
+        courant = courant -> suiv;
 
-    T_noeud *temp = current->suiv;  // Le dernier client
+    T_noeud *dernier = courant -> suiv;
+    int *auvegarde = dernier -> data
+    courant -> suiv = NULL;
 
-    // Sauvegarder les données du dernier client
-    *sauvegarde = temp->data;
-
-    current->suiv = NULL;  // Le nouvel avant-dernier élément pointe vers NULL
-    free(temp);  // Libère la mémoire du dernier client
+    free(dernier;)
+    return sauvegarde;
 }
 
 
+// comptage du nbr de clients dans la file
 
-void pop_client(struct T_noeud *tete)
+int compte_client(T_noeud *tete)
 {
-    if ((*tete) -> suiv == NULL)
+    int compteur = 0;
+    T_noeud courant = *tete;
+
+    while(courant != NULL)
     {
-        int client = (*tete) -> 
-    }  
+        compteur++;
+        courant = courant -> suiv;
+    }
+    return compteur;
+}
+
+
+// Verifie si un operateur est libre
+
+int* ops_libre(int* tableau)
+{
+    int* libre = malloc(NBR_OPS * sizeof(int));
+    for(int i = 0; i < NBR_OPS; i++)
+    {
+        if (tableau[i] > 0)
+            libre[i] = 1;
+        else 
+            libre[i] = 0;
+    }
+    return libre;
 }
 
 int main(void)
 {
     srand(time(NULL));
     
-    for(i = 0; i <= NBR_JOUR; i++)
+    T_noeud *file_attente;
+    file_attente -> suiv = NULL;
+
+    for(int i = 0; i <= NBR_JOUR; i++)
     {   
         
 
-        for(j = 0; j <= NBR_HEURES * 3600; i++)
+        for(int j = 0; j <= NBR_HEURES * 3600; i++)
         {
             // creation centre appel
             int simu_centre_appel[NBR_OPS] = {0};
 
+            // tableau contenant l'heure d'arrivee des clients
+            int *heure_arrivee = simu_arrivee();
 
+            // regarde si un client est arrive
+            if (compare_tps(j, heure_arrivee))
+            {
+                struct Client client;
+                client.h_arrivee = j;
+                ajout_client(*file_attente, client);
+            }
+
+            // regarde s'il y a des gens dans la file d'attente & si un operateur est libre
+            // si oui, enregistre son tps de debut de prise en charge + genere une duree aleatoire de conversation client/ops
+            int compteur = compte_client(*file_attente);
+            int *libre = ops_libre(simu_centre_appel);
+            for(int k = 0; k < NBR_OPS; k++)
+            {
+                if ((libre[k] == 0) && (compteur >= 1))
+                {
+                    compteur = compteur - 1;
+                    struct Client client = pop_client(*file_attente);
+                    client.debut_prise_en_charge = j;
+                    simu_centre_appel[k] = tps_prise_en_charge();
+                }
+            }
+            
+            // simule la seconde passee
+            for(int l = 0; l < NBR_OPS; l++)
+            {
+                if (simu_centre_appel[l] > 0)
+                    simu_centre_appel[l] = simu_centre_appel[l] - 1;
+            }
         }
     }
 }
