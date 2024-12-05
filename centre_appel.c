970d92fb1dd1559d69dfe31b2255cbc2f2e64f6c
Pokimino <159826843+Pokimino@users.noreply.github.com>
2024-12-05 19:54:56 +0100

    Add files via upload

--- centre_appel.c
+++ centre_appel.c
@@ -5,14 +5,41 @@
 #include <stdbool.h>
 
 
-// def constantes
+// def constantes modifiable
 
-#define NBR_OPS 5
-#define NBR_HEURES 11
-#define NBR_JOUR 1
-#define minsrv 100 // en secondes
-#define maxsrv 600
-#define lambda 0.06
+#define NBR_OPS 5       // Nombre d'operateur
+#define NBR_HEURES 11   // Duree de la journee
+#define NBR_JOUR 1      // Nombre de jour de la simulation
+#define minsrv 100      // Duree min du service
+#define maxsrv 600      // Duree max du service
+#define lambda 0.06     // lambda
+
+
+// Structure et liste chainee
+
+struct Client {
+    int h_arrivee;
+    int debut_prise_en_charge;
+    int fin_prise_en_charge;
+} ;
+
+typedef struct T_noeud {
+    struct Client data;
+    struct T_noeud *suiv;
+} T_noeud;
+
+
+//Fonctions utilise dans le programme  et leur role:
+
+double intervalle_tps(void);                                // Renvoie la duree entre l'appel de 2 clients
+int* simu_arrivee(void);                                    // Cree et renvoie un tableau contenant les heures d'arrive des differents clients
+bool compare_tps(int j, int *tab);                          // comparaison entre tableau de simu_arrivee et l'heure actuelle
+int tps_prise_en_charge(void);                              // Renvoie la duree de prise en charge, (choisit au hasard entre minsrv et maxsrv)
+void ajout_client(T_noeud *file, struct Client nv_client);  // Ajoute un client a la liste chainee
+T_noeud pop_client(T_noeud *tete);                           // Retire un client et renvoie les donnes du clients (Heure_prise en charge,Duree attente ....)
+int compte_client(T_noeud *tete);                           // Compte le nombre de clients present dans la file ??? File d'attente 
+int* ops_libre(int* tableau);                               // Prend un tableau contenant les operateurs, et determine si ils sont libres ou pas
+int convertisseur_tps(int n);                               // Transforme un entier en seconde et renvoie , l'heure, la minute et la seconde.
 
 
 // Intervalle de temps separant deux clients
@@ -28,7 +55,7 @@ double intervalle_tps(void)
 
 // Simulation tps d'arrivee des clients
 
-int* simu_arrivee()
+int* simu_arrivee(void)
 {
     int i = 0;
     int* tableau_tps_arrivee = malloc(10000 * sizeof(int));
@@ -43,7 +70,7 @@ int* simu_arrivee()
     return tableau_tps_arrivee;
 }
 
-// comparaison entre tableau de simu_arrivee et l'heure actuelle
+// Comparaison entre tableau de simu_arrivee et l'heure actuelle
 
 bool compare_tps(int j, int *tab)
 {
@@ -56,25 +83,12 @@ bool compare_tps(int j, int *tab)
 
 // tps de prise en charge client
 
-int tps_prise_en_charge()
+int tps_prise_en_charge(void)
 {
     int tps_avec_client = rand() % maxsrv + minsrv;
     return tps_avec_client;
 }
 
-
-// creation d'une file sous forme d'une liste chainee
-
-struct Client {
-    int h_arrivee;
-    int debut_prise_en_charge;
-    int fin_prise_en_charge;
-} ;
-
-typedef struct T_noeud {
-    struct Client data;
-    struct T_noeud *suiv;
-} T_noeud;
 
 
 // ajout d'un client dans la file
@@ -86,16 +100,16 @@ void ajout_client(T_noeud *file, struct Client nv_client)
     nouveau = ((T_noeud *) malloc(sizeof(T_noeud)));
 
     nouveau -> data = nv_client;
-    nouveau -> suiv = *file;
-    *file = nouveau;
+    nouveau -> suiv = file;
+    file = nouveau;
 }
 
 
 // retirer un client de la file
 
-struct pop_client(T_noeud *tete) 
+T_noeud pop_client(T_noeud *tete) 
 {
-    if (*tete == NULL)
+    if ( tete == NULL)
         return;
 
     if ((*tete) -> suiv == NULL) 
@@ -140,6 +154,7 @@ int compte_client(T_noeud *tete)
 int* ops_libre(int* tableau)
 {
     int* libre = malloc(NBR_OPS * sizeof(int));
+    
     for(int i = 0; i < NBR_OPS; i++)
     {
         if (tableau[i] > 0)
@@ -150,21 +165,21 @@ int* ops_libre(int* tableau)
     return libre;
 }
 
-
+// Converti un temp en seconde en heure/minute/seconde
 int convertisseur_tps(int n)
 {
     int heures, minutes, secondes;
     heures = n / 3600;
     n %= 3600;
-    minutes = total_secondes / 60;
-    secondes = total_secondes % 60;
+    minutes = n / 60;
+    secondes = n % 60;
     return heures, minutes, secondes;
 }
 
 
 int main(void)
 {
-    srand(time(NULL));
+    srand(time(NULL));  // initialise pour l'utilisation de random
     
     T_noeud *file_attente;
     file_attente -> suiv = NULL;
@@ -182,7 +197,8 @@ int main(void)
     float taux_client_pris_en_charge;
     int h_fin_de_service;
 
-    // creation centre appel
+    // creation centre appel ; un tableau contenant les operateur, 1 si ils sont occupe, 0 si ils sont libres
+
     int simu_centre_appel[NBR_OPS] = {0};
 
     for(int i = 0; i <= NBR_JOUR; i++)
